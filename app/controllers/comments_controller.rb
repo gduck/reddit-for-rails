9462db559898355d467a1840c879090309080983
gduck <dale.github@thefoos.net>
2014-12-17 14:46:09 +0800

    dynamic comments, comment partials working

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -8,20 +8,33 @@ class CommentsController < ApplicationController
     # creates an instance of the record
     # that hasn't been saved yet
     #comment = Comment.new(comment_params)
-    @comment = current_user.comments.new(comment_params)
+    @comment = Comment.new(
+      :comment => params[:comment][:comment],
+      :post_id => params[:comment][:post_id],
+      :user_id => current_user.id)
 
     if @comment.save
+      #old way is to render stuff back to ajax
+      # render :json => {
+      #   :data => @comment,
+      #   :email => current_user.email
+      #   }, :status => 201
+
+
       # this says we need to render pages in the following formats
       respond_to do |format|
+
         # order matters
         # in Rails convention is we need to create a file called 'create.js.erb' insdie the comments view folder
         format.js
-        puts ">>>>>>>>>>>>>>>>> comment #{@comment.comment}"
-        # format.html {redirect_to @comment.post}
+        format.html {redirect_to @comment.post}
       end
       # redirect_to comment.post
     else
-      redirect_to :back
+      respond_to do |format|
+        format.js { render 'fail.js.erb' }
+        format.html { redirect_to :back }
+      end
     end
 
   end
@@ -38,6 +51,7 @@ class CommentsController < ApplicationController
     def comment_params
       params.require(:comment).permit(:post_id, :comment)
     end
+
 
 
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,7 @@
 class Comment < ActiveRecord::Base
 
+  validates :comment, :length => { maximum: 140 }, :presence => true
+
   belongs_to :user
   belongs_to :post
 end
